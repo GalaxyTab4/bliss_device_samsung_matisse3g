2ce4ac0be890aa4e9c474dc97fe9c9696791ae58
Jackeagle <jackeagle102@gmail.com>
2015-07-11 10:54:25 -0400

    s3ve3g : Bliss Initial Setup [2/2]
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- bliss.mk
+++ bliss.mk
@@ -12,12 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/s3ve3g/full_s3ve3g.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := GT-I9301I
-PRODUCT_NAME := cm_s3ve3g
+PRODUCT_NAME := bliss_s3ve3g
