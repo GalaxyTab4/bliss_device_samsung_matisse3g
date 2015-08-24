c3eb080c590fcc668e992da543ccde16d2e12d5c
sub77 <sub77@ymail.com>
2015-08-24 05:58:10 +0200

    bootloader assert

--- releasetools.py
+++ releasetools.py
@@ -13,7 +13,27 @@
 # limitations under the License.
 #
 
-""" Custom OTA commands for matisse devices """
+import re
+import os
+
+TARGET_DIR = os.getenv('OUT')
+
+def FullOTA_Assertions(info):
+  AddBootloaderAssertion(info, info.input_zip)
+
+
+def IncrementalOTA_Assertions(info):
+  AddBootloaderAssertion(info, info.target_zip)
+
+
+def AddBootloaderAssertion(info, input_zip):
+  android_info = input_zip.read("OTA/android-info.txt")
+  m = re.search(r"require\s+version-bootloader\s*=\s*(\S+)", android_info)
+  if m:
+    bootloaders = m.group(1).split("|")
+    if "*" not in bootloaders:
+      info.script.AssertSomeBootloader(*bootloaders)
+    info.metadata["pre-bootloader"] = m.group(1)
 
 def FullOTA_InstallEnd(info):
   info.script.Mount("/system")
