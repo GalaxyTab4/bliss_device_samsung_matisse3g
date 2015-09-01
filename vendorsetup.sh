34ee54f5b5976d8d44dcb1843407eed77b96a023
sub77 <sub77@ymail.com>
2015-09-01 22:17:13 +0200

    moved patchdir to common

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # Run device patches on the tree
-sh device/samsung/matisse3g/patches/apply.sh
+#sh device/samsung/matisse3g/patches/apply.sh
 
 add_lunch_combo cm_matisse3g-eng
 add_lunch_combo cm_matisse3g-userdebug
